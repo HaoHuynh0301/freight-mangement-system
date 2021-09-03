baa26be6c3fb45103d57184458627698b33755f7
DESKTOP-9NGS0J6\hao15 <hao152903@gmail.com>
2021-09-03 19:24:25 +0700

    feat[react-native]: create order page UI (1).

--- cus_driver_app/screens/Orders.js
+++ cus_driver_app/screens/Orders.js
@@ -4,24 +4,95 @@ import {
     Text,
     StyleSheet,
     SafeAreaView,
+    FlatList
 } from "react-native";
+import {
+    headerFontSize,
+    backIcon
+} from '../contants';
 
 class Orders extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            orders: [
+                {
+                    name: 'One',
+                    id: 1
+                },
+                {
+                    name: 'Two',
+                    id: 2
+                }
+            ],
+            selectedData: ''
+        }
+    }
+
+    renderHeader() {
+        return(
+            <View style={styles.headerContainer}>
+                <Text style={styles.userInformationText}>Đơn hàng</Text>
+            </View>
+        );
+    }
+
+    renderMainView() {
+        const renderItem = ({item}) => {
+            return(
+                <View>
+                    <Text>{item.name}</Text>
+                </View>
+            );
+        }
+
+        const renderEmptyFavoriteProducts = () => {
+            return(
+                <View>
+                    <Text>Empty</Text>
+                </View>
+            );
+        }
+
+        return(
+            <View>
+                <FlatList
+                    data = {this.state.orders}
+                    showsVerticalScrollIndicator = {false}
+                    keyExtractor = {(item) => item.id}
+                    renderItem = {renderItem}
+                    extraData = {this.state.selectedData}
+                    contentContainerStyle = {{padding: 10, marginTop: 15}}
+                    ListEmptyComponent = {renderEmptyFavoriteProducts}
+                />
+            </View>
+        );
     }
 
     render() {
         return(
             <SafeAreaView>
-                <Text>Order</Text>
+                {this.renderHeader()}
+                {this.renderMainView()}
             </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
+    headerContainer: {
+        height: 70,
+        width: '100%',
+        flexDirection: 'row',
+        backgroundColor: '#ff7733',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    userInformationText: {
+        fontSize: headerFontSize,
+        fontWeight: 'bold',
+        marginLeft: 10
+    }
 });
 
 export default Orders;
