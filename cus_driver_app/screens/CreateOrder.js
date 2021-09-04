cd7de8b64196ca588630c8900a9090964ed7bc0e
DESKTOP-9NGS0J6\hao15 <hao152903@gmail.com>
2021-09-04 16:23:09 +0700

    style[react-native]: style css for create order page UI (1).

--- cus_driver_app/screens/CreateOrder.js
+++ cus_driver_app/screens/CreateOrder.js
@@ -5,17 +5,26 @@ import {
     Text,
     TouchableOpacity,
     Image,
-    StyleSheet
+    StyleSheet,
+    TextInput,
+    ScrollView
 } from 'react-native';
 import {
     greyColor,
     headerFontSize,
-    xIcon
+    xIcon,
+    appFontSize,
+    callIcon, 
+    homeIcon,
+    accountIcon
 } from '../contants';
 
 class CreateOrder extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            orderSize: this.props.route.params.status
+        }
     }
 
     renderHeader() {
@@ -31,7 +40,60 @@ class CreateOrder extends Component {
                         style={styles.xIconStyle}
                     ></Image>
                 </TouchableOpacity>
+                <View style={styles.titleWrapper}>
+                    <Text style={styles.userInformationText}>TẠO ĐƠN HÀNG</Text>
+                </View>
             </View>
+        );
+    }
+
+    renderMainView() {
+        return(
+            <ScrollView>
+                <View style={styles.createOrderMainView}>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'flex-start'
+                    }}>
+                        <Text style={styles.titleFontSize}>NGƯỜI NHẬN</Text>
+                    </View>
+                    <View style={styles.shippingLocationInforWrapper}>
+                        <View style={styles.locationInforDetail}>
+                            <Image
+                                source = {callIcon}
+                                style={styles.iconLocationInfor}
+                            ></Image>
+                            <TextInput
+                                placeholder = 'Nhập số điện thoại khách hàng'
+                                style={styles.inputStyle}
+                            ></TextInput>
+                        </View>
+                        <View style={styles.locationInforDetail}>
+                            <Image
+                                source = {accountIcon}
+                                style={styles.iconLocationInfor}
+                            ></Image>
+                            <TextInput
+                                placeholder = 'Tên khách hàng'
+                                style={styles.inputStyle}
+                            ></TextInput>
+                        </View>
+                        <View style={styles.locationInforDetail}>
+                            <Image
+                                source = {homeIcon}
+                                style={styles.iconLocationInfor}
+                            ></Image>
+                            <TextInput
+                                placeholder = 'Địa chỉ chi tiết (nhà/ngõ/ngách)'
+                                style={styles.inputStyle}
+                            ></TextInput>
+                        </View>
+                    </View>
+                    <View style={styles.locationPickerWrapper}>
+
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 
@@ -39,6 +101,7 @@ class CreateOrder extends Component {
         return(
             <SafeAreaView>
                 {this.renderHeader()}
+                {this.renderMainView()}
             </SafeAreaView>
         );
     }
@@ -58,12 +121,59 @@ const styles = StyleSheet.create({
     userInformationText: {
         fontSize: headerFontSize,
         fontWeight: 'bold',
-        marginLeft: 10
+        marginLeft: 10,
+        alignSelf: 'center'
     },
     xIconStyle: {
         height: 20,
         width: 25,
         marginLeft: 10
+    },
+    titleWrapper: {
+        paddingLeft: 90
+    },
+    createOrderMainView: {
+        flexDirection: 'column',
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginTop: 15
+    },
+    titleFontSize: {
+        fontSize: appFontSize,
+        fontWeight: 'bold'
+    },
+    appFontSize: {
+        fontSize: appFontSize
+    },
+    shippingLocationInforWrapper: {
+        flexDirection: 'column',
+        marginTop: 5,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        width: '100%',
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    iconLocationInfor: {
+        height: 20,
+        width: 20,
+    },
+    locationInforDetail: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputStyle: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: greyColor,
+        width: '100%',
+        paddingLeft: 10,
+        fontSize: appFontSize
+    },
+    locationPickerWrapper: {
+        flexDirection: 'column',
+        alignItems: 'center'
     }
 });
 
