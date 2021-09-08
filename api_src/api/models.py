cbb36000c93be5af88123cec9dca71544b2aa106
DESKTOP-9NGS0J6\hao15 <hao152903@gmail.com>
2021-09-08 14:48:59 +0700

    feat[django]: create Cusomter User table in database.

--- api_src/api/models.py
+++ api_src/api/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
-# Create your models here.
+class Sex(models.Model):
+    name = models.CharField(max_length = 255, unique = True, null = False)
+    
+    def __str__(self):
+        return self.name
+    
+
+class MyUserManager(BaseUserManager):
+    pass
+
+
+class Customer(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name = 'email address',
+        max_length = 255,
+        unique = True,
+    )
+    customer_name = models.CharField(max_length = 255, unique = True, null = True)
+    date_of_birth = models.CharField(max_length = 20)
+    sex = models.ForeignKey(Sex, related_name = 'customers_sex', on_delete = models.CASCADE)
+    phone_numner = models.IntegerField()
+    address = models.CharField(max_length = 255, null = False)
+    province = models.CharField(max_length = 255, null = False)
+    district = models.CharField(max_length = 255, null = False)
+    ward = models.CharField(max_length = 255, null = False)
+    bank_name = models.CharField(max_length = 255, unique = True, null = True)
+    bank_number = models.IntegerField()
+    bank_provine = models.CharField(max_length = 255, null = False)
+    is_active = models.BooleanField(default = True)
+    is_admin = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
+    
+    objects = MyUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+                        'customer_name', 
+                        'date_of_birth', 
+                        'sex', 
+                        'phone_numner', 
+                        'address',
+                        'province',
+                        'district',
+                        'ward',
+                        'bank_name',
+                        'bank_number',
+                        'bank_provine',
+                        ]
+    
+    def __str__(self):
+        return str(self.email)
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
+    
+    def has_perm(self, perm, obj = None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True   
--- api_src/api/views.py
+++ api_src/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.serializers import Serializer
+from . import models, serializers
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
+from rest_framework import permissions, status
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
