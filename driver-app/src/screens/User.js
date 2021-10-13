bb77cef15e7aee432112caae449902bed7080e6a
DESKTOP-9NGS0J6\hao15 <hao152903@gmail.com>
2021-10-13 20:10:06 +0700

    feat[reactjs]: create user's information page (1).

--- driver-app/src/screens/User.js
+++ driver-app/src/screens/User.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './css/userPage.css';
 import axios from "axios";
 import {
     Link,
@@ -15,7 +16,13 @@ class User extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            driverInformation: {}
+            driverInformation: {},
+
+            // User information input
+            name: '',
+            email: '',
+            phonenumber: '',
+            driverLicense: ''
         }
 
         this.getDriverInformation  = this.getDriverInformation.bind(this);
@@ -54,11 +61,10 @@ class User extends Component {
 
                 {/* Title Wrapper */}
                 <div style = {{
-                    width: '60%',
+                    width: '40%',
                     // textAlign: 'center'
                 }}>
                     <p style = {{
-                        
                         marginTop: '50px',
                         fontSize: '30px',
                         fontWeight: 'bold',
@@ -69,9 +75,123 @@ class User extends Component {
 
                 {/* User's information Wrapper */}
                 <div style = {{
-                    width: '60%',
+                    width: '40%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    marginTop: '15px'
                 }}>
-
+                    
+                    {/* Name Wrapper */}
+                    <div style = {{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        marginBottom: '20px'
+                    }}>
+                        <p style = {{fontWeight: 'bold'}}>Tên tài xế</p>
+                        <input 
+                            style = {{
+                                borderRadius: '10px',
+                                padding: '5px',
+                                border: 'solid 0.5px grey',
+                                backgroundColor: '#f5f5f0'
+                            }} placeholder = {this.state.driverInformation.name} 
+                            value = {this.state.name}
+                            onChange = {(event) => {
+                                this.setState({
+                                    name: event.target.value
+                                });
+                            }}
+                        ></input>
+                    </div>
+                    <div style = {{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        marginBottom: '20px'
+                    }}>
+                        <p style = {{fontWeight: 'bold'}}>Email</p>
+                        <input 
+                            style = {{
+                                borderRadius: '10px',
+                                padding: '5px',
+                                border: 'solid 0.5px grey',
+                                backgroundColor: '#f5f5f0'
+                            }} placeholder = {this.state.driverInformation.email} 
+                            value = {this.state.email}
+                            onChange = {(event) => {
+                                this.setState({
+                                    email: event.target.value
+                                });
+                            }}
+                        ></input>
+                    </div>
+                    <div style = {{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        marginBottom: '20px'
+                    }}>
+                        <p style = {{fontWeight: 'bold'}}>Số điện thoại</p>
+                        <input 
+                            style = {{
+                                borderRadius: '10px',
+                                padding: '5px',
+                                border: 'solid 0.5px grey',
+                                backgroundColor: '#f5f5f0'
+                            }} placeholder = {this.state.driverInformation.phone_number} 
+                            value = {this.state.phonenumber}
+                            onChange = {(event) => {
+                                this.setState({
+                                    phonenumber: event.target.value
+                                });
+                            }}
+                        ></input>
+                    </div>
+                    <div style = {{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        marginBottom: '20px'
+                    }}>
+                        <p style = {{fontWeight: 'bold'}}>Bằng lái xe</p>
+                        <input 
+                            style = {{
+                                borderRadius: '10px',
+                                padding: '5px',
+                                border: 'solid 0.5px grey',
+                                backgroundColor: '#f5f5f0'
+                            }} placeholder = {this.state.driverInformation.driverLicense} 
+                            value = {this.state.driverLicense}
+                            onChange = {(event) => {
+                                this.setState({
+                                    driverLicense: event.target.value
+                                });
+                            }}
+                        ></input>
+                    </div>
+                    <div style = {{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        marginBottom: '20px'
+                    }}>
+                        <p style = {{fontWeight: 'bold'}}>Mật khẩu</p>
+                        <input 
+                            type = 'password'
+                            style = {{
+                                borderRadius: '10px',
+                                padding: '5px',
+                                border: 'solid 0.5px grey',
+                                backgroundColor: '#f5f5f0'
+                            }} 
+                            placeholder = {this.state.driverInformation.driverLicense} 
+                            value = {this.state.driverLicense}
+                            onChange = {(event) => {
+                                this.setState({
+                                    driverLicense: event.target.value
+                                });
+                            }}
+                        ></input>
+                    </div>
+                    <button>
+                        <p>Lưu thông tin</p>
+                    </button>
                 </div>
             </div>
         );
