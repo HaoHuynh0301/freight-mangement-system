91fa6a1e66aee192a9b4dc379a979536d1806adf
DESKTOP-9NGS0J6\hao15 <hao152903@gmail.com>
2021-11-01 18:54:13 +0700

    feat[reactjs]: create protected route and define auth class.

--- driver-app/src/auth.js
+++ driver-app/src/auth.js
@@ -1,17 +1,40 @@
+import {
+    ipAddress
+} from './contants';
 const axios = require('axios');
 const localStorage = require('local-storage');
 
 class Auth {
     constructor() {
-        this.authenticate = false;
+        let token = localStorage.get('token');
+        let isAuth = false;
+        if(token !== null) {
+            axios.get(`${ipAddress}/api/driver-middleware/`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then((response) => {
+                isAuth = true;
+                localStorage.set('token', response.data.access_token);
+            })
+            .catch(error => {
+                console.log('Error!');
+            });
+        }
+
+        this.authenticate = isAuth;
     }
 
-    login(cb) {
+    login(token, cb) {
+        localStorage.set('token', token);
         this.authenticate = true;
         cb();
     }
 
     logout(cb) {
+        localStorage.set('token', null);
         this.authenticate = false;
         cb();
     }
