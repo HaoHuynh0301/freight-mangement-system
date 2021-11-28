9ea5753d0894c04db18eab70fa9c5af461faf7f5
HaoHuynh0301 <hao152903@gmail.com>
2021-11-28 12:49:16 +0700

    refactor[django]: Add avatar properties to customer models.

--- api_src/api/models.py
+++ api_src/api/models.py
@@ -98,6 +98,7 @@ class Customer(AbstractBaseUser):
     is_active = models.BooleanField(default = True)
     is_admin = models.BooleanField(default = False)
     is_superuser = models.BooleanField(default = False)
+    avatar = models.ImageField(upload_to="product-image/", blank=True)
     
     objects = MyUserManager()
     
--- api_src/api_src/settings.py
+++ api_src/api_src/settings.py
@@ -143,9 +143,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'api.Customer'
 
